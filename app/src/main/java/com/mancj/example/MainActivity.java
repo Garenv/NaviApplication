7bec90e946d253fc474266ff8aeba091beac83b4
darc0n3 <darcell.swinton@gmail.com>
2017-12-05 11:18:34 -0500

    Deleted stuff from main

--- app/src/main/java/com/mancj/example/MainActivity.java
+++ app/src/main/java/com/mancj/example/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { 
 
         super.onCreate(savedInstanceState);
 
